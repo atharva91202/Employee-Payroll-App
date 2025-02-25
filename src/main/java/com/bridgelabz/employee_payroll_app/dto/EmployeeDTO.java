f48f03283d0a567193b8d9a5ad08f6f0e4a877f6
Atharva <atharvasharma.cs25@gmail.com>
2025-02-25 12:10:37 +0530

    merged uc3 to master

---
+++ src/main/java/com/bridgelabz/employee_payroll_app/dto/EmployeeDTO.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.employee_payroll_app.dto;
+
+public class EmployeeDTO {
+    private String name;
+    private double salary;
+
+    public EmployeeDTO(String name, double salary){
+        this.name=name;
+        this.salary=salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
