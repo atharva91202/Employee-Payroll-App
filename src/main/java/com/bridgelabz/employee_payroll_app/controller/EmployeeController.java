8c88b12f1e118c0d9726dfbc93280b3c2cdaf097
Atharva <atharvasharma.cs25@gmail.com>
2025-02-25 11:10:18 +0530

    [Atharva] Add. Added CURD for database

--- src/main/java/com/bridgelabz/employee_payroll_app/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employee_payroll_app/controller/EmployeeController.java
@@ -1,14 +1,38 @@
 package com.bridgelabz.employee_payroll_app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/employeepayrollservice")
+@RequestMapping("/employees")
 public class EmployeeController {
+    @GetMapping("/test")
+    public String testAPI() {
+        return "Employee Payroll REST API is working!";
+    }
+
     @GetMapping
-    public String testEndpoint() {
-        return "Welcome to Employee Payroll App";
+    public String getEmployye(){
+        return "Fetching employee";
+    }
+
+    @GetMapping("/all")
+    public String getAllEmployye(){
+        return "Fetching all employees";
+    }
+
+    @PostMapping()
+    public String addEmployee() {
+        return "Employee added!";
+    }
+
+    @PutMapping("/update")
+    public String updateEmployee() {
+        return "Employee updated!";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteEmployee() {
+        return "Employee deleted!";
     }
 }
