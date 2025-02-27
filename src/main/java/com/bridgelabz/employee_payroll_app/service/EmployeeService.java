60773c1c7cabeb5a3e76a4c058d6baee789c41f4
Atharva <atharvasharma.cs25@gmail.com>
2025-02-27 15:51:31 +0530

    merged uc7 to master

--- src/main/java/com/bridgelabz/employee_payroll_app/service/EmployeeService.java
+++ src/main/java/com/bridgelabz/employee_payroll_app/service/EmployeeService.java
@@ -1,5 +1,5 @@
 package com.bridgelabz.employee_payroll_app.service;
-
+import lombok.extern.slf4j.Slf4j;
 import com.bridgelabz.employee_payroll_app.model.Employee;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -8,50 +8,65 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class EmployeeService {
-    private List<Employee> employeeList = new ArrayList<>();
+    private final List<Employee> employeeList = new ArrayList<>();
     private long idCounter = 1;
 
     // Create an Employee
     public ResponseEntity<Employee> addEmployee(Employee employee) {
         employee.setId((int) idCounter++);
         employeeList.add(employee);
+        log.info("Added Employee : {}",employee);
         return ResponseEntity.ok(employee);
     }
 
     // Get Employee by ID
     public ResponseEntity<Employee> getEmployeeById(Long id) {
+        log.info("Fectching Employee with ID: {}",id);
         Optional<Employee> employee = employeeList.stream()
                 .filter(emp -> emp.getId() == id.intValue())
                 .findFirst();
-        return employee.map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+        if (employee.isPresent()) {
+            log.info("Employee found: {}", employee.get());
+            return ResponseEntity.ok(employee.get());
+        } else {
+            log.warn("Employee with ID {} not found", id);
+            return ResponseEntity.notFound().build();
+        }
     }
 
     // Get all Employees
     public ResponseEntity<List<Employee>> getAllEmployees() {
+        log.info("Fetching all employee. Total count: {}",employeeList.size());
         return ResponseEntity.ok(employeeList);
     }
 
     // Update Employee
     public ResponseEntity<Employee> updateEmployee(Long id, Employee updatedEmployee) {
+        log.info("Updating Employee with ID:{}",id);
         for (Employee emp : employeeList) {
             if (emp.getId() == id.intValue()) {
                 emp.setName(updatedEmployee.getName());
                 emp.setSalary(updatedEmployee.getSalary());
+                log.info("Updated Employee:{}",emp);
                 return ResponseEntity.ok(emp);
             }
         }
+        log.warn("Employee with ID {} not found for update",id);
         return ResponseEntity.notFound().build();
     }
 
     // Delete Employee
     public ResponseEntity<String> deleteEmployee(Long id) {
+        log.info("Deleting Employee with ID:{}",id);
         boolean removed = employeeList.removeIf(emp -> emp.getId() == id.intValue());
         if (removed) {
+            log.info("Employee with ID {} deleted successfully",id);
             return ResponseEntity.ok("Employee deleted successfully for ID: " + id);
         }
+        log.warn("Employee with ID {} not found for deletion",id);
         return ResponseEntity.notFound().build();
     }
 }
