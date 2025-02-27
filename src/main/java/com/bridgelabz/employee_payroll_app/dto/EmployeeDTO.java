12a6df4744e309109b815a54df6243ec6ef5f00a
Atharva <atharvasharma.cs25@gmail.com>
2025-02-27 13:45:08 +0530

    [Atharva] Add. Added Lombok for auto-generating getter and setter in DTO

--- src/main/java/com/bridgelabz/employee_payroll_app/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employee_payroll_app/dto/EmployeeDTO.java
@@ -1,19 +1,16 @@
 package com.bridgelabz.employee_payroll_app.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
     private String name;
     private double salary;
 
-    public EmployeeDTO(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
 }
