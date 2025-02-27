57e372517d92c99b505e01584d19d1c2431daf3d
Atharva <atharvasharma.cs25@gmail.com>
2025-02-27 13:45:08 +0530

    merged uc6 to master

--- src/main/java/com/bridgelabz/employee_payroll_app/model/Employee.java
+++ src/main/java/com/bridgelabz/employee_payroll_app/model/Employee.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,38 +19,4 @@ public class Employee {
     private String name;
     private double salary;
 
-    // Default constructor (Required for JPA)
-    public Employee() {}
-
-    // Parameterized constructor
-    public Employee(int id, String name, double salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
